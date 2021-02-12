424063e6fc7addb43fcd5a9f081b08498392455a
philip <s102279@ap.be>
2021-02-12 19:11:04 +0200

    niet kleiner dan nul

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ export default function App() {
     {id: 1, label: 'female' },
   ];
 
+   
 
   const [weight, setWeight] = useState('');
   const [time, settime] = useState(1);
@@ -26,7 +27,7 @@ export default function App() {
   const [gender, setGender] = useState('male');
   const [promiles, setPromiles] = useState(0);
 
-  function calculate(){
+  function bloodLevel(){
     let result = 0;
     let liters = bottles * 0.33;
     let grams = liters * 8 * 4.5;
@@ -49,7 +50,12 @@ export default function App() {
       result = gramsL / (weight * 0.6);
 
     }
-    setPromiles(result)
+
+    if(result>=0){
+      setPromiles(result)
+    }else{
+      setPromiles(0)
+    }
   }
 
 
@@ -75,7 +81,6 @@ export default function App() {
 
         </Picker>
       </View>
-
       <View>
         <Text style={styles.textTitle}>Time</Text>
         <Picker
@@ -89,15 +94,36 @@ export default function App() {
           <Picker.Item label="3 hours" value={3} />
           <Picker.Item label="4 hours" value={4} />
           <Picker.Item label="5 hours" value={5} />
+          <Picker.Item label="6 hours" value={6} />
+          <Picker.Item label="7 hours" value={7} />
+          <Picker.Item label="8 hours" value={8} />
+          <Picker.Item label="9 hours" value={9} />
+          <Picker.Item label="10 hours" value={10} />
+          <Picker.Item label="11 hours" value={11} />
+          <Picker.Item label="12 hours" value={12} />
+          <Picker.Item label="13 hours" value={13} />
+          <Picker.Item label="14 hours" value={14} />
+          <Picker.Item label="15 hours" value={15} />
+          <Picker.Item label="16 hours" value={16} />
+          <Picker.Item label="17 hours" value={17} />
+          <Picker.Item label="18 hours" value={18} />
+          <Picker.Item label="19 hours" value={19} />
+          <Picker.Item label="20 hours" value={20} />
+          <Picker.Item label="17 hours" value={21} />
+          <Picker.Item label="18 hours" value={22} />
+          <Picker.Item label="19 hours" value={23} />
+          <Picker.Item label="20 hours" value={24} />
 
         </Picker>
       </View>
       <View>
         <Text style={styles.textTitle}>gender</Text>
-        <RadioGroup
-                  options={radiogroup_options}
-                  onChange={(option) => setGender(option)}
-            />
+        <RadioForm
+          radio_props={radio_props}
+          initial={0}
+          onPress={(value) => setGender(value)}
+        >
+        </RadioForm>
       </View>
 
       <View>
@@ -107,7 +133,7 @@ export default function App() {
       </View>
         <Button
           title="CALCULATE"
-          onPress={calculate}
+          onPress={bloodLevel}
         />
       </View>
       
